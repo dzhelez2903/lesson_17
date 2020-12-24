b7b6e7563a0a25c39f5e5ef7b3cfff810188275f
dzhelez2903 <dzhelez@yahoo.com>
2020-12-25 01:15:34 +0200

    added js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* change div*/
+link1 = document.getElementById("joineUs");
+link1.onclick = function () {
+  document.getElementById("block1").style.display = "none";
+  document.getElementById("block2").style.display = "block";
+  console.log(link1);
+};
+link2 = document.getElementById("signIn");
+link2.onclick = function () {
+  document.getElementById("block1").style.display = "block";
+  document.getElementById("block2").style.display = "none";
+};
