9865fef50e6d2f8421553259ea5dcf38b4ccfe2e
dzhelez2903 <dzhelez@yahoo.com>
2020-12-25 15:40:14 +0200

    added block №3

--- js/main.js
+++ js/main.js
@@ -3,11 +3,27 @@ link1 = document.getElementById("joineUs");
 link1.onclick = function () {
   document.getElementById("block1").style.display = "none";
   document.getElementById("block2").style.display = "flex";
-  console.log(link1);
+  document.getElementById("block3").style.display = "none";
+  //console.log(link1);
 };
 link2 = document.getElementById("signIn");
 link2.onclick = function () {
   document.getElementById("block1").style.display = "flex";
   document.getElementById("block2").style.display = "none";
-  console.log(link2);
+  document.getElementById("block3").style.display = "none";
+  //console.log(link2);
+};
+link3 = document.getElementById("pass");
+link3.onclick = function () {
+  document.getElementById("block1").style.display = "none";
+  document.getElementById("block2").style.display = "none";
+  document.getElementById("block3").style.display = "flex";
+  //console.log(link3);
+};
+link4 = document.getElementById("create");
+link4.onclick = function () {
+  document.getElementById("block1").style.display = "none";
+  document.getElementById("block2").style.display = "flex";
+  document.getElementById("block3").style.display = "none";
+  //console.log(link4);
 };
