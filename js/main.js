a254487397360bf66352d0504506f8fe80b6b27f
dzhelez2903 <dzhelez@yahoo.com>
2020-12-25 12:21:01 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 link1 = document.getElementById("joineUs");
 link1.onclick = function () {
   document.getElementById("block1").style.display = "none";
-  document.getElementById("block2").style.display = "block";
+  document.getElementById("block2").style.display = "flex";
   console.log(link1);
 };
 link2 = document.getElementById("signIn");
 link2.onclick = function () {
-  document.getElementById("block1").style.display = "block";
+  document.getElementById("block1").style.display = "flex";
   document.getElementById("block2").style.display = "none";
   console.log(link2);
 };
